19df8d9f7994e93d630f4b102ccc8cf938a7cbbf
ksajan <ksajan12@gmail.com>
2024-05-06 11:22:09 +0530

    refactor & fix: added f string and correct use of double quotes under them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from src.route import router
+from api.route import router
 
 app = FastAPI()
 
--- src/portfolio.py
+++ src/portfolio.py
@@ -1,5 +1,6 @@
-import loader as loader
 import requests
+
+import loader as loader
 
 
 class DriftTrade:
@@ -7,16 +8,16 @@ class DriftTrade:
         self.positions = None
         self.profit_n_loss = None
         self.liquidation_price = None
-        self.drift_http_url = loader.constants
+        self.drift_http_url = loader.constants.DRIFT_API_URL
 
     def get_markets(self):
-        url = self.drift_http_url + "/v2/markets"
+        url = f"{self.drift_http_url}/v2/markets"
         response = requests.get(url)
         self.markets = response.json()
         return self.markets
 
     def get_positions(self, marketIndex=None, marketType=None):
-        url = self.drift_http_url + "/v2/positions"
+        url = f"{self.drift_http_url}/v2/positions"
         if marketIndex is not None and marketType is not None:
             payload = {"marketIndex": marketIndex, "marketType": marketType}
         else:
@@ -30,11 +31,15 @@ class DriftTrade:
         profit_n_loss = 0
         liquidation_price = 0
         for position in self.positions["perp"]:
-            temp_url = f"{self.drift_http_url}/v2/positionInfo/{position["marketIndex"]}"
+            temp_url = (
+                f"{self.drift_http_url}/v2/positionInfo/{position['marketIndex']}"
+            )
             response = requests.get(temp_url)
             position_info = response.json()
-            profit_n_loss += position_info["unrealizedPnl"] + position_info["unsettledPnl"]
+            profit_n_loss += (
+                position_info["unrealizedPnl"] + position_info["unsettledPnl"]
+            )
             liquidation_price = position_info["liquidationPrice"]
         self.profit_n_loss = profit_n_loss
         self.liquidation_price = liquidation_price
-        return 
+        return
